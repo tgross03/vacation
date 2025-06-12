7e3361f320debc71d215aa54f572a719eae82598
tom_g <tom@edgetom.dev>
2025-06-12 22:10:28 +0200

    Initial project structure

---
+++ vacation/version.py
@@ -0,0 +1,45 @@
+# This file was adapted from https://github.com/radionets-project/pyvisgen/blob/main/pyproject.toml
+# pyvisgen is licensed under MIT License
+#
+# MIT License
+#
+# Copyright (c) 2021 radionets-project
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+## This is adapted from https://github.com/astropy/astropy/blob/master/astropy/version.py
+## see https://github.com/astropy/astropy/pull/10774 for a discussion on why this needed.
+
+try:
+    try:
+        from ._dev_version import version
+    except ImportError:
+        from ._version import version
+except Exception:
+    import warnings
+
+    warnings.warn(
+        "Could not determine vacation version. This indicates"
+        " a broken installation. Please install simtools from"
+        " the local git repository."
+    )
+    del warnings
+    version = "0.0.0"
+
+__version__ = version
