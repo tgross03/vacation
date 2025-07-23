5d9d274b33ebb80a913984a63e34dc52a57a56d6
Tom Groß <tom.gross@udo.edu>
2025-07-23 16:33:34 +0200

    Fix random forest optim

--- vacation/scripts/random_forest.py
+++ vacation/scripts/random_forest.py
@@ -4,14 +4,13 @@ from pathlib import Path
 import joblib
 import numpy as np
 import torchvision.transforms as T
+from scipy.stats import randint
 from skimage.feature import hog
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from tqdm import tqdm
 
 from vacation.data import GalaxyDataset
-
-rng = np.random.default_rng(seed=1337)
 
 parser = argparse.ArgumentParser(
     description="Extracting HOG features and hyperparameter optimization for a Random Forest."
@@ -114,7 +113,7 @@ if num_npy_files < 4:
 
     # original dataset
     print("Extracting HOG features from original dataset...")
-    X_train, y_train = hog_features(train_ds, augmented=False)
+    X_train, y_train, _ = hog_features(train_ds, augmented=False)
 
     # saving features and labels
     np.save(str(location / "rf_features_train_aug.npy"), X_train_aug)
@@ -127,11 +126,11 @@ if num_npy_files < 4:
 else:
     # loading if not features already saved
     X_train_aug = np.load(str(location / "rf_features_train_aug.npy"))
-    y_train_aug = np.load(str(location / "../notebooks/rf_labels_train_aug.npy"))
-    X_val_aug = np.load(str(location / "../notebooks/rf_features_valid_aug.npy"))
-    y_val_aug = np.load(str(location / "../notebooks/rf_labels_valid_aug.npy"))
-    X_train = np.load(str(location / "../notebooks/rf_features_train.npy"))
-    y_train = np.load(str(location / "../notebooks/rf_labels_train.npy"))
+    y_train_aug = np.load(str(location / "rf_labels_train_aug.npy"))
+    X_val_aug = np.load(str(location / "rf_features_valid_aug.npy"))
+    y_val_aug = np.load(str(location / "rf_labels_valid_aug.npy"))
+    X_train = np.load(str(location / "rf_features_train.npy"))
+    y_train = np.load(str(location / "rf_labels_train.npy"))
 
 
 # hyperparameter tuning for Random Forest
@@ -140,11 +139,11 @@ RF = RandomForestClassifier(random_state=42)
 
 # parameter distribution for optimization
 param_dist = {
-    "n_estimators": rng.integers(50, 400),
+    "n_estimators": randint(50, 400),
     "max_features": ["sqrt", "log2", None],
-    "max_depth": rng.integers(1, 20),
-    "min_samples_split": rng.integers(2, 10),
-    "min_samples_leaf": rng.integers(1, 2),
+    "max_depth": randint(1, 20),
+    "min_samples_split": randint(2, 10),
+    "min_samples_leaf": randint(1, 2),
 }
 
 rand_search = RandomizedSearchCV(
